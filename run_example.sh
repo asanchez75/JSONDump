0b5e9d9543378805140608d427f70e139acf37e9
ryan <ryan@ubuntu.ubuntu-domain>
2017-06-30 10:19:16 +0530

    fixed config upload issue

--- run_example.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$SPARK_HOME/bin/spark-submit --class org.dbpedia.convert.Example --master local[2]  --files conf/application.conf target/json-dump-1.0-jar-with-dependencies.jar
--- src/main/scala/org/dbpedia/convert/utils/Configuration.scala
+++ src/main/scala/org/dbpedia/convert/utils/Configuration.scala
@@ -2,12 +2,12 @@ package org.dbpedia.convert.utils
 
 import java.io.FileInputStream
 import java.util.Properties
+import org.apache.spark.SparkFiles
 
 object Configuration {
 
-  
   private val properties = new Properties()
-  properties.load(new FileInputStream(Constants.CONFIG_DIR))
+  properties.load(new FileInputStream(SparkFiles.get(Constants.CONFIG_DIR)))
 	 
   lazy val inputFiles: String = properties.getProperty(Constants.DATA_INPUT);
   
