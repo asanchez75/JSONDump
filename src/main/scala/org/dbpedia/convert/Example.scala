b47adb0d25ae864acb13f5e9ed40a52bf21c7d4e
¨Dimitris <d.kontokostas@geophy.com>
2017-10-04 16:19:27 +0300

    draft json code

--- src/main/scala/org/dbpedia/convert/Example.scala
+++ src/main/scala/org/dbpedia/convert/Example.scala
@@ -1,7 +1,7 @@
 package org.dbpedia.convert;
+import org.apache.jena.rdf.model.ResourceFactory
 import org.apache.spark.{SparkConf, SparkContext}
 import org.dbpedia.extraction.destinations.Quad
-
 import org.dbpedia.convert.api.Configuration
 
 object Example {
@@ -14,24 +14,58 @@ object Example {
     // Load our input data.
     val input =  sc.textFile(Configuration.inputFile)
     input
+    input
       //Sort all lines as strings
-      .sortBy(identity)
+      //.sortBy(s => s)
       // convert lines to Quads
-      .flatMap(line => {
-      line match {
-        case Quad(quad) => Some(quad)
-        case _ => None
+      .filter(s => !s.startsWith("#"))
+      .flatMap( str => {
+        try {
+          str match {
+            case Quad(quad) => Some(quad)
+            case _ => None
+          }
+        } catch {
+          case e: Exception => None
+        }
       }
-    })
-      // Group quads by subject
+      )
+      .filter(_.subject.nonEmpty)
+      .map( q => JsonVal(q.subject, getJsonKey(q), q.value))
+      .sortBy(_.subject)
       .groupBy(_.subject)
-      // have them again sorted to save them as sorted
+      .map ( vals => jsonValsToJson(vals._1, vals._2))
+
+      .saveAsTextFile("/home/dimitris/data/dbpedia/export.jsonl")
+
+
+  }
+
+  case class JsonVal (subject: String, key: String, value: String)
+  case class JsonVals (subject: String, key: String, values: Seq[String])
+
+  def getJsonKey(quad: Quad): String = {
+    val key = ResourceFactory.createProperty(quad.predicate).getLocalName
+    if (quad.datatype != null && quad.datatype.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#langString")) key + "_" + quad.language else key
+  }
+
+  import com.fasterxml.jackson.databind.ObjectMapper
+  import com.fasterxml.jackson.module.scala.DefaultScalaModule
+  val objectMapper = new ObjectMapper()
+  objectMapper.registerModule(DefaultScalaModule)
+
+  def jsonValsToJson(subject: String, vals: Iterable[JsonVal]): String = {
+
+    val values: Map[String, Any] = vals.toList
+      .map(v => (v.key, v.value))
       .sortBy(_._1)
-      // quads should be converted to json-ld
-      .foreach{
-      case (subject, quads) => {
-        println( subject)
-        quads.foreach(println(_))
-      }}
+      .groupBy(_._1)
+      .map(g =>
+        g._1 -> {if (g._2.size == 1) g._2.head._2 else  g._2.map(_._2).distinct.toSeq})
+
+
+    val jstring  = objectMapper.writeValueAsString( Map("@id" -> subject) ++ values)
+    println(jstring)
+    jstring.replaceAll("\n", "")
   }
 }
